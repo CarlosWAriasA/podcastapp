a107ef10ccd47982af147aceb484418fd836ec68
Carlos Arias <carlos.ariasalmanzar@gmail.com>
2024-10-20 19:51:58 -0400

    x

---
+++ app/{root}/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <main>
+        <p className="text-white-1">LEFT SIDE BAR</p>
+        {children}
+        <p className="text-white-1">RIGHT SIDEBAR</p>
+      </main>
+    </div>
+  );
+}
