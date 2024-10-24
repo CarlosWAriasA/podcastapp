d1e52df05b9b73d5e96254e5c28a2b21c7f4163e
Carlos Arias <carlos.ariasalmanzar@gmail.com>
2024-10-23 23:00:51 -0400

    Home Page layout finish

---
+++ components/RightSideBar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RightSideBar = () => {
+  return (
+    <section className="right_sidebar text-white-1 ">Right Side Bar</section>
+  );
+};
+export default RightSideBar;
